adc8cc988c3f0cc162e91c66f20fcdfcd3fdd28e
Darren Emory <darren.emory@inspireinvesting.com>
2022-06-30 17:46:37 -0600

    progress

--- src/components/CreatedTopic.tsx
+++ src/components/CreatedTopic.tsx
@@ -2,9 +2,8 @@ import { Box, Button, Heading } from "grommet";
 import { Add, FormAdd, FormSubtract, Tools } from "grommet-icons";
 
 interface CreatedTopicProps {
-  name: string;
-  duration: number;
-  id: number;
+  topic: any;
+  calculateTopicWeight: (topic: any, increase?: boolean) => void;
 }
 
 function CreatedTopic(props: CreatedTopicProps) {
@@ -13,7 +12,7 @@ function CreatedTopic(props: CreatedTopicProps) {
       <Box pad="small" direction="column" gap="small">
         <Box>
           <Heading level="1" textAlign="center" margin={{ vertical: "medium" }}>
-            {props.name}
+            {props.topic.name}
           </Heading>
         </Box>
 
@@ -22,7 +21,9 @@ function CreatedTopic(props: CreatedTopicProps) {
             size="small"
             plain={undefined}
             icon={<FormSubtract size="medium" style={{ width: 20 }} />}
-            onClick={() => {}}
+            onClick={() => {
+              props.calculateTopicWeight(props.topic);
+            }}
             primary
             style={{ width: 30, height: 30, padding: "3px 5px" }}
           />
@@ -31,14 +32,16 @@ function CreatedTopic(props: CreatedTopicProps) {
             textAlign="center"
             margin={{ vertical: "none", horizontal: "small" }}
           >
-            {props.duration} Minutes
+            {props.topic.topicDuration} Minutes, {props.topic.weight}
           </Heading>
 
           <Button
             size="small"
             plain={undefined}
             icon={<FormAdd size="medium" style={{ width: 20 }} />}
-            onClick={() => {}}
+            onClick={() => {
+              props.calculateTopicWeight(props.topic, true);
+            }}
             primary
             style={{ width: 30, height: 30, padding: "3px 5px" }}
           />
--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -17,15 +17,18 @@ import {
 import CreatedTopic from "../components/CreatedTopic";
 
 interface MeetingTopic {
-  id: any;
   name: string;
   weight: number;
+  topicDuration: number | boolean;
+  [key: string]: string | number | boolean;
 }
 
 function Create() {
   const [meetingDuration, setMeetingDuration] = useState<number>();
   const [topicDuration, setTopicDuration] = useState<any>();
-  const [meetingTopics, setMeetingTopics] = useState({});
+  const [meetingTopics, setMeetingTopics] = useState<
+    Array<MeetingTopic | null>
+  >([]);
   const [topicInput, setTopicInput] = useState("");
 
   useEffect(() => {
@@ -56,19 +59,61 @@ function Create() {
   };
 
   const handleAddMeetingTopic = (topicName: string) => {
-    let newTopic = {
-      id: uuid(),
+    let newTopic: MeetingTopic = {
       name: topicName,
+      weight: 2,
+      topicDuration: false,
+      key: uuid(),
     };
-    let updatedMeetingTopics: any = { ...meetingTopics };
-    updatedMeetingTopics[newTopic.id] = newTopic;
+    let updatedMeetingTopics = meetingTopics;
+    updatedMeetingTopics.push(newTopic);
 
     setMeetingTopics(updatedMeetingTopics);
     calculateTopicDuration();
   };
 
+  const calculateTopicWeight = (topic: MeetingTopic, increase?: boolean) => {
+    let updatedTopic: MeetingTopic = topic;
+
+    if (increase && topic.weight < 3) {
+      updatedTopic.weight++;
+    } else if (!increase && topic.weight > 1) {
+      updatedTopic.weight--;
+    }
+
+    updateTopic(updatedTopic);
+  };
+
+  const updateTopic = (topic: MeetingTopic) => {
+    const newTopics = meetingTopics.map((t) => {
+      // 👇️ if id equals 2 replace object
+      if (t?.key === topic.key) {
+        return {
+          name: topic.name,
+          weight: topic.weight,
+          topicDuration: topic.topicDuration,
+          key: topic.key,
+        };
+      }
+
+      // 👇️ otherwise return object as is
+      return t;
+    });
+
+    setMeetingTopics(newTopics);
+  };
+
   const calculateTopicDuration = () => {
     let totalTopics = Object.keys(meetingTopics).length;
+
+    let weightedTopics = [];
+    for (let w = 3; w > 0; w--) {
+      weightedTopics[w] = meetingTopics.filter((topic) => topic?.weight === w);
+    }
+    // [1 = weighted 3]
+    // let topicsWeighted3 =
+    // starting with weight 3, divide meetingDuration by half, one half going to weight 3 topics (divded evenly), and other half going to next weight, 2, and 1
+
     if (meetingDuration) {
       let topicDuration: any = meetingDuration / totalTopics;
       if (topicDuration % 2 !== 0) {
@@ -82,74 +127,74 @@ function Create() {
 
   return (
     <AppWrapper>
-      <Main
-        direction="column"
-        pad={{ bottom: "large" }}
-        align="center"
-        background="brand"
-      >
-        <Heading>Let's set up your awesome meeting:</Heading>
-        <Card background="light-1" pad="medium" width="medium">
-          <Box align="center" pad={{ vertical: "medium" }}>
-            <Text margin={{ vertical: "small" }}>
-              How long is your meeting?
-            </Text>
-            <Select
-              onChange={({ option }) => handleMeetingDuration(option)}
-              options={[
-                "15 minutes",
-                "30 minutes",
-                "45 minutes",
-                "1 hour",
-                "1.5 hours",
-                "2 hours",
-              ]}
-            />
-          </Box>
-          <Box align="center" pad={{ vertical: "medium" }}>
-            <Text margin={{ vertical: "small" }}>
-              What topics do you want to discuss?
-            </Text>
-            <Tip content="Press Enter to add topics">
-              <input
-                type="text"
-                className="textInput"
-                value={topicInput}
-                onChange={(e) =>
-                  setTopicInput((e.target as HTMLInputElement).value)
-                }
-                onKeyDown={(e) => {
-                  if (e.key === "Enter") {
-                    handleAddMeetingTopic((e.target as HTMLInputElement).value);
-                    setTopicInput("");
-                  }
-                }}
-              />
-            </Tip>
-          </Box>
-
-          {Object.keys(meetingTopics).length > 0 && (
+      <Heading>Let's set up your awesome meeting:</Heading>
+      <Main direction="column" background="brand" align="center">
+        <Box direction="row">
+          <Card background="light-1" pad="medium" width="medium">
             <Box align="center" pad={{ vertical: "medium" }}>
-              <Heading
-                textAlign="center"
-                margin={{ vertical: "small" }}
-                level="3"
-              >
-                Your awesome meeting agenda:
-              </Heading>
-              <Box>
-                {Object.values(meetingTopics).map((topic: any) => (
-                  <CreatedTopic
-                    key={topic.id}
-                    id={topic.id}
-                    name={topic.name}
-                    duration={topicDuration}
-                  />
-                ))}
-              </Box>
+              <Text margin={{ vertical: "small" }}>
+                How long is your meeting?
+              </Text>
+              <Select
+                onChange={({ option }) => handleMeetingDuration(option)}
+                options={[
+                  "15 minutes",
+                  "30 minutes",
+                  "45 minutes",
+                  "1 hour",
+                  "1.5 hours",
+                  "2 hours",
+                ]}
+              />
             </Box>
-          )}
-        </Card>
+            <Box align="center" pad={{ vertical: "medium" }}>
+              <Text margin={{ vertical: "small" }}>
+                What topics do you want to discuss?
+              </Text>
+              <Tip content="Press Enter to add topics">
+                <input
+                  type="text"
+                  className="textInput"
+                  value={topicInput}
+                  onChange={(e) =>
+                    setTopicInput((e.target as HTMLInputElement).value)
+                  }
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                      handleAddMeetingTopic(
+                        (e.target as HTMLInputElement).value
+                      );
+                      setTopicInput("");
+                    }
+                  }}
+                />
+              </Tip>
+            </Box>
+          </Card>
+
+          <Card background="light-1" pad="medium" width="medium">
+            {Object.keys(meetingTopics).length > 0 && (
+              <Box align="center" pad={{ vertical: "medium" }}>
+                <Heading
+                  textAlign="center"
+                  margin={{ vertical: "small" }}
+                  level="3"
+                >
+                  Your awesome meeting agenda:
+                </Heading>
+                <Box>
+                  {Object.values(meetingTopics).map((topic: any) => (
+                    <CreatedTopic
+                      key={topic.key}
+                      topic={topic}
+                      calculateTopicWeight={calculateTopicWeight}
+                    />
+                  ))}
+                </Box>
+              </Box>
+            )}
+          </Card>
+        </Box>
         <Box align="" pad={{ vertical: "medium" }}>
           <Link to="/create">
             <Button
